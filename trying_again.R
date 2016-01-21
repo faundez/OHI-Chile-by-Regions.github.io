fbdddc0a9aa70860ad9a8c810597e5339aae49ac
adam1993 <yifuqian@umail.ucsb.edu>
2016-01-20 16:09:54 -0800

    I think there is some errors with the numbers, so I changed some of them, and enjoy :)

--- trying_again.R
+++ trying_again.R
@@ -1,6 +1,6 @@
 # create a data frame from scratch 
-age <- c(25, 30, 56)
+age <- c(25, 30, 54)
 gender <- c("male", "female", "male")
-weight <- c(160, 110, 220) 
+weight <- c(160, 116, 220) 
 mydata <- data.frame(age,gender,weight)
 plot(x=age, y=weight)
